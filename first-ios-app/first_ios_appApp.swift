2dcc9fa7af4fcd97446f8d1ecf66817dd0c84f43
Cihat Salik <cihatsalik1@gmail.com>
2022-09-11 00:12:44 +0300

    Initial Commit

---
+++ first-ios-app/first_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_ios_appApp.swift
+//  first-ios-app
+//
+//  Created by Cihat Salik on 11.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct first_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
